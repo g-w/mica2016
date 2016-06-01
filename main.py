b9dde1888e9b0993345fb2c8b72e00bcf32cde3c
Gregor Weckbecker <gregor@feiner-fug.net>
2016-06-01 22:39:01 +0200

    wip

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from telnetlib import *
 from random import *
-import time
+from telnetlib import *
+import itertools
 import re
+import time
+
+import numpy as np
+import scipy.sparse.csgraph as ng
 
 server_encoding = 'UTF-8'
 
@@ -17,18 +21,29 @@ class ServerConnection:
         return self.telnet.read_until(b'\n').decode(server_encoding)
 
     def write(self, message):
-        return self.telnet.write(bytes(message + '\n', encoding=server_encoding))
+        if message:
+            return self.telnet.write(bytes(message + '\n', encoding=server_encoding))
 
 class GameMap:
     def __init__(self, str, map_size_x, map_size_y):
         self.str = str
-        self.map_size_x = map_size_x
-        self.map_size_y = map_size_y
+        self.size_x = map_size_x
+        self.size_y = map_size_y
 
     def __getitem__(self, pos):
         x, y = pos
-        index = 2 * x + y * 2 * self.map_size_x
+        index = 2 * x + y * 2 * self.size_x
         return self.str[index]
+
+    def is_move_possible(self, score, move):
+        new_position = (score[2] + move[0], score[3] + move[1])
+
+        result = 0 <= new_position[0] and new_position[0] < self.size_x and \
+            0 <= new_position[1] and new_position[1] < self.size_y and \
+            (self[new_position[0], new_position[1]] != 'w' and \
+            self[new_position[0], new_position[1]] != 'W')
+
+        return result
 
 name = choice(['Graham', 'John', 'Terry', 'Eric', 'Terry', 'Michael']) + ' ' + choice(['Chapman', 'Cleese', 'Gilliam', 'Idle', 'Jones', 'Palin'])
 print('Hello my name is', name)
@@ -56,27 +71,10 @@ timeout = 0.0
 
 scores = []
 
+my_index = None
+
 current_map_str = ""
 current_map = None
-
-def select_possible_moves(position, moves):
-    def move_possible(move):
-        field = 0
-        if move == 'w' and position[3] > 0:
-            field = current_map[position[2], position[3] - 1]
-        elif move == 'a' and position[2] > 0:
-            field = current_map[position[2] - 1, position[3]]
-        elif move == 's' and position[3] < map_size_y - 1:
-            field = current_map[position[2], position[3] + 1]
-        elif move == 'd' and position[2] < map_size_x - 1:
-            field = current_map[position[2]  + 1, position[3]]
-
-        return field != 'w' and field != 'W'
-
-    return filter(move_possible, moves)
-
-def select_first_possible_move(position, moves):
-    return next(select_possible_moves(position, moves))
 
 while ongoing:
     str = connection.read_line()
@@ -128,37 +126,96 @@ while ongoing:
 
         scores.append((player_name, score, position_x, position_y, is_fox))
 
+        if player_name == name:
+            my_index = len(scores) - 1
+
     elif str.startswith('wfyc'):
-        time.sleep(0.01)
+        possible_moves = {
+            'n': (0, 0),
+            'w': (0, -1),
+            's': (0, 1),
+            'a': (-1, 0),
+            'd': (1, 0)
+        }
 
-        my_score = next(filter(lambda score: score[0] == name, scores))
-        fox_score = next(filter(lambda score: score[4], scores))
+        possible_moves_keys = list(possible_moves.keys())
 
-        if my_score == fox_score:
-            action = choice(['w', 's', 'a', 'd'])
-            log('I am the fox. My turn is ' + action)
+        def distance(score, another_score):
+            return abs(score[2] - another_score[2]) + abs(score[3] - another_score[3])
 
-            connection.write(action)
-        else:
-            delta_x = my_score[2] - fox_score[2]
-            delta_y = my_score[3] - fox_score[3]
+        def rate(scores):
+            my_score = next(filter(lambda score: score[0] == name, scores))
+            fox_score = next(filter(lambda score: score[4], scores))
 
-            moves = []
-            if abs(delta_x) > abs(delta_y):
-                moves.append('a' if delta_x > 0 else 'd')
-                moves.append('w' if delta_y > 0 else 's')
-                moves.append('d' if delta_x > 0 else 'a')
-                moves.append('s' if delta_y > 0 else 'w')
+            is_fox = my_score == fox_score
+
+            max_distance = current_map.size_x + current_map.size_y
+            if is_fox:
+                min_distance = min([ distance(score, my_score) for score in scores if score[0] != name])
+                return 1.0 -  min_distance / max_distance
+            return distance(my_score, fox_score) / max_distance
+
+        def select_move(scores, n):
+            if n == max(current_map.size_x, current_map.size_y) / 4:
+                return 2, None
+
+            best_rating = 1
+            best = None
+
+            permutations = None
+            if n < 3:
+                permutations = itertools.permutations(possible_moves, len(scores))
             else:
-                moves.append('w' if delta_y > 0 else 's')
-                moves.append('a' if delta_x > 0 else 'd')
-                moves.append('s' if delta_y > 0 else 'w')
-                moves.append('d' if delta_x > 0 else 'a')
+                permutations = []
+                for move in possible_moves:
+                    permutation = ['n'] * len(scores)
+                    permutation[my_index] = move
+                    permutations.append(permutation)
 
-            action = select_first_possible_move(my_score, moves)
-            log('my turn is ' + action)
+            for moves in permutations:
+                new_scores = []
+                rating = 0
 
-            connection.write(action)
+                my_move = None
+                for score, move in zip(scores, moves):
+                    delta = possible_moves[move]
+                    if current_map.is_move_possible(score, delta):
+                        x = score[2] + delta[0]
+                        y = score[3] + delta[1]
+
+                        if score[0] == name:
+                            my_move = move
+
+                        new_scores.append((score[0], score[1], x, y, score[4]))
+
+                    else:
+                        rating = -1
+                        break
+
+                if rating == -1:
+                    continue
+
+                rating = 0.9 * rate(new_scores) + 0.1 * random()
+                if rating == 0:
+                    print('this shouldnot happen!')
+                    return (rating, my_move)
+
+                bt_rating, bt_move = select_move(new_scores, n + 1)
+
+                if bt_rating != 2:
+                    rating = bt_rating
+
+                if best_rating > rating:
+                    best_rating = rating
+                    best = my_move
+
+            return (best_rating, best)
+
+        rating, move = select_move(scores, 0)
+
+        log("my choice", rating, move)
+
+        connection.write(move)
 
     # terminate the loop if game is over
     elif str.startswith('game is over'):
